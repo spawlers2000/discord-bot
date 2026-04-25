d026d30957288aa9fea7a787ea67b761d4908ddf
spawlers2000 <spawlers2000@gmail.com>
2026-04-25 16:49:41 +0800

    fix db system

--- index.js
+++ index.js
@@ -198,8 +198,8 @@ client.on(Events.InteractionCreate, async (interaction) => {
     const uid = interaction.user.id;
     const now = Date().toISOString();
 
-    const end = Date(Number(event.endTime)).toISOString();
-    const start = Date(Number(event.eventTime)).toISOString();
+    const end = Number(event.endTime);
+    const start = Number(event.eventTime);
 
     // ==========================
     // 🔥 最重要：截止判斷（一定放最上面）
