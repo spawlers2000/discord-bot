5e7d1d24d8573d3a20b4c317d2a5480943e3ba04
spawlers2000 <spawlers2000@gmail.com>
2026-04-25 17:16:37 +0800

    fix db system

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,11 @@
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
-const { token, clientId, guildId } = require('./config.json');
+require('dotenv').config();
 
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+ 
 const commands = [
   new SlashCommandBuilder()
     .setName('event')
